1dcab6720130553f1acce9eeefebcaceae760603
reki630 <43027045+reki630@users.noreply.github.com>
2019-03-24 16:39:51 +0800

    add functions

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,51 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
+    <alphabet :cities="cities" @change="handleLetterChange"></alphabet>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/CityHeader'
+import CitySearch from './components/CItySearch'
+import CityList from './components/CityList'
+import Alphabet from './components/Alphabet'
+
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch,
+    CityList,
+    Alphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json').then(this.handleGetCityInfoSuccess)
+    },
+    handleGetCityInfoSuccess (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        this.cities = res.data.cities
+        this.hotCities = res.data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
